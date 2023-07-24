45862b8b180bd77e69f536d11b1b6ecf460f5b78
ryan2135 <ryan2135@pacificu.edu>
2023-07-24 15:01:14 -0700

    Added static functions

---
+++ js/static.js
@@ -0,0 +1,12 @@
+class MyString {
+  static isLower (str) {
+    return str.toLowerCase () === str;
+  }
+
+  static countVowels (str) {
+    
+  }
+}
+
+console.log (MyString.isLower ("abc"));
+console.log (MyString.isLower ("abC"));
