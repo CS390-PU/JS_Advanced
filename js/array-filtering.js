20a76b84f9821ca5cdc4c265523a316e073d30dd
ryan2135 <ryan2135@pacificu.edu>
2023-07-25 16:23:51 -0700

    Added diff filter functions

--- js/array-filtering.js
+++ js/array-filtering.js
@@ -5,5 +5,16 @@ function getPassingGrades (scores, cutoff) {
   return scores.filter (isPassing);
 }
 
-const passingScores = getPassingGrades ([95.5, 76.3, 52.4, 60.0, 88.5, 76.0], 60.0);
+const scores = [95.5, 76.3, 52.4, 60.0, 88.5, 76.0];
+
+let passingScores = getPassingGrades (scores, 60.0);
 console.log ("Passing Scores: " + passingScores);
+console.log (typeof (passingScores));
+
+passingScores = scores.filter (score => score >= 60);
+console.log ("Passing Scores: ", passingScores);
+console.log (typeof (passingScores));
+
+passingScores = scores.filter ((score) => score >= 60.0);
+console.log ("Passing Scores: ", passingScores);
+console.log (typeof (passingScores));
